e86d52a557201084204a3214d8a563ad7358295e
CamShoward <>
2021-04-19 02:58:09 +0000

    Implemented basic command functionality.

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 import discord
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
+client = discord.Client()
 
-    async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
 
-client = MyClient()
-client.run('Put token here')
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello my name is FredBot. I am a discord bot built by the Suny Fredonia Computer Science Club!')
+    
+    if message.content.startswith('$website'):
+        await message.channel.send('The link to our website is http://cs.fredonia.edu/~csclub/')
+    
+    if message.content.startswith('$wholesome'):
+      await message.channel.send('You are not a drop in the ocean, you are the entire ocean in a drop.')
+    
+    if message.content.startswith('$help'):
+      await message.channel.send('Happy to help. To start my prefix is $ put that in front of any of the listed commands to activate me. \n My current supported commands include: \n $hello \n $website \n $wholesome \n $help')
+client.run('your toekn here')
